6954b9500e6e9b94b5f9ec5af6696857056ecbba
Leenday <iammilagre@gmail.com>
2020-10-26 19:23:12 +0300

    add sign in/up

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,11 @@
+class Api::TasksController < Api::ApplicationController
+  def index; end
+
+  def show; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
+end
---
+++ test/controllers/web/session_controller_test.rb
@@ -0,0 +1,15 @@
+# test 'should post create' do
+#   password = generate(:string)
+#   user = create(:user, { password: password })
+#   attrs = {
+#     email: user.email,
+#     password: password,
+#   }
+#   post :create, params: { session_form: attrs }
+#   assert_response :redirect
+# end
+
+# test 'should delete destroy' do
+#   delete :destroy
+#   assert_response :redirect
+# end
