e346bbe9ed77f49fa52daa3dd089ea84197809c8
Leenday <iammilagre@gmail.com>
2020-10-28 18:57:25 +0300

    adds authorization

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,4 +1,13 @@
 class Admin::ApplicationController < ApplicationController
   include AuthHelper
   helper_method :current_user
+  before_action :authenticate_user!, :authorize
+
+  def authorize
+    render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false) if (forbidden?)
+  end
+
+  def forbidden?
+    !current_user.is_a?(Admin)
+  end
 end
--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionController::TestCase
+  setup do
+    admin = create(:admin)
+    sign_in admin
+  end
+
   test 'should get show' do
     user = create(:user)
     get :show, params: { id: user.id }
