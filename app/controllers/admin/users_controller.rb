8ea99b8928df142d02e522924c45c4de67a4fcdd
Leenday <iammilagre@gmail.com>
2020-10-28 20:18:42 +0300

    adds CRU for users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,4 +7,36 @@ class Admin::UsersController < Admin::ApplicationController
     @q = User.ransack(params[:q])
     @users = @q.result.order(:type).order(:id).page(params[:page]).per(10)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_url
+    else
+      render :new_admin_user_url
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_url
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :type)
+  end
 end
--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -5,4 +5,28 @@ class Admin::UsersControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  test 'should get edit' do
+    user = create(:user)
+    get :edit, params: { id: user.id }
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+  end
+ 
+  test "should post create" do
+    user = attributes_for(:user)
+    post :create, params: { user: user }
+    assert_response :redirect
+  end
+
+  test "should patch update" do
+    user = create(:user)
+    user_attrs = attributes_for(:user)
+    patch :update, params: { id: user.id, user: user_attrs }
+    assert_response :redirect
+  end
 end
