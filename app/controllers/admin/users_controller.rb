b837f05886126dee69753141fab69b8b2e889926
Leenday <iammilagre@gmail.com>
2020-10-28 20:00:46 +0300

    users list

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,9 @@ class Admin::UsersController < Admin::ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def index
+    @q = User.ransack(params[:q])
+    @users = @q.result.order(:type).order(:id).page(params[:page]).per(10)
+  end
 end
