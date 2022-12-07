95662817779ab9530d763ca4baa2e5cbfe9008d4
Leenday <iammilagre@gmail.com>
2022-12-07 10:07:15 +0600

    adds a controllers with actions

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,2 +1,2 @@
-class Api::ApplicationController < ApplicationController
+class ApplicationController < ActionController::Base
 end
--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,11 +1,46 @@
-class Api::TasksController < Api::ApplicationController
-  def index; end
+class Api::V1::TasksController < Api::V1::ApplicationController
+  respond_to :json
 
-  def show; end
+  def show
+    task = Task.find(params[:id])
 
-  def create; end
+    respond_with(task, serializer: TaskSerializer)
+  end
 
-  def update; end
+  def index
+    tasks = Task.all
+                .ransack(ransack_params)
+                .result
+                .page(page)
+                .per(per_page)
 
-  def destroy; end
+    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
+
+  def create
+    task = current_user.my_tasks.new(task_params)
+    task.save
+
+    respond_with(task, serializer: TaskSerializer, location: nil)
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+
+    respond_with(task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :author_id, :assignee_id, :state_event, :expired_at)
+  end
 end
