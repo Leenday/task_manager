64dd2add69f987e2c2a8c2c902dd7ad9b04a6d22
Leenday <iammilagre@gmail.com>
2023-01-03 12:56:38 +0600

    adds letter_opener and previews for mails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root to: 'web/boards#show'
 
   scope module: :web do
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,26 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def task_created
+    user = User.first
+    task = Task.first
+    params = { user: user, task: task }
 
+    UserMailer.with(params).task_created
+  end
+
+  def task_updated
+    user = User.first
+    task = Task.first
+    params = { user: user, task: task }
+
+    UserMailer.with(params).task_updated
+  end
+
+  def task_deleted
+    user = User.first
+    task = Task.first
+    params = { user: user, task: task }
+
+    UserMailer.with(params).task_deleted
+  end
 end
